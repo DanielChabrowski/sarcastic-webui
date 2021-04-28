5be22955dcb53445fd758ac52bb0fe096560f38a
Daniel Chabrowski <danchabrowski@gmail.com>
2021-04-28 04:48:44 +0200

    Add pre-commit, editorconfig, prettier

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
 currently_playing = document.getElementById("currently-playing");
 
 control_play = document.getElementById("control-play");
-control_play.onclick = function() {
+control_play.onclick = function () {
     let uuid = currently_playing.uuid;
     let msg = JSON.stringify({
         Action: {
-            Play: uuid
-        }
-    })
-    socket.send(msg)
-}
+            Play: uuid,
+        },
+    });
+    socket.send(msg);
+};
 
 control_stop = document.getElementById("control-stop");
-control_stop.onclick = function() {
+control_stop.onclick = function () {
     let msg = JSON.stringify({
-        Action: "Stop"
-    })
-    socket.send(msg)
-}
+        Action: "Stop",
+    });
+    socket.send(msg);
+};
 
 control_pause = document.getElementById("control-pause");
-control_pause.onclick = function() {
+control_pause.onclick = function () {
     let msg = JSON.stringify({
-        Action: "Pause"
-    })
-    socket.send(msg)
-}
+        Action: "Pause",
+    });
+    socket.send(msg);
+};
 
 let socket = new WebSocket("ws://192.168.1.3:9023");
 
-socket.onopen = function(e) {
+socket.onopen = function (e) {
     console.log("[WebSocket] Connection established");
     console.log("[WebSocket] Sending to server");
-    socket.send("{\"QueryResources\":{}}");
+    socket.send('{"QueryResources":{}}');
 };
 
-socket.onmessage = function(event) {
+socket.onmessage = function (event) {
     console.log(`[WebSocket] Data received from server: ${event.data}`);
 
-    response = JSON.parse(event.data)
+    response = JSON.parse(event.data);
     if (response.Resources) {
         resources_div = document.getElementById("resources");
         resources_div.innerHTML = "";
 
-        response.Resources.forEach(r => {
-            var res_uri = document.createElement('a');
+        response.Resources.forEach((r) => {
+            var res_uri = document.createElement("a");
             res_uri.innerHTML = r.name;
-            res_uri.onclick = function() {
+            res_uri.onclick = function () {
                 let uuid = r.uuid;
                 let msg = JSON.stringify({
                     Action: {
-                        Play: uuid
-                    }
-                })
-                console.log(msg)
+                        Play: uuid,
+                    },
+                });
+                console.log(msg);
                 currently_playing.innerHTML = r.name;
                 currently_playing.uuid = uuid;
                 socket.send(msg);
-            }
+            };
 
-            var res_element = document.createElement('div');
+            var res_element = document.createElement("div");
             res_element.appendChild(res_uri);
 
             resources_div.appendChild(res_element);
@@ -67,16 +67,16 @@ socket.onmessage = function(event) {
     }
 };
 
-socket.onclose = function(event) {
-    document.body.innerHTML = "<div class='server-error'>Could not connect to server</div>"
+socket.onclose = function (event) {
+    document.body.innerHTML = "<div class='server-error'>Could not connect to server</div>";
 
     if (event.wasClean) {
         console.log(`[WebSocket] Connection closed, code=${event.code} reason=${event.reason}`);
     } else {
-        console.log('[WebSocket] Connection died');
+        console.log("[WebSocket] Connection died");
     }
 };
 
-socket.onerror = function(error) {
+socket.onerror = function (error) {
     console.log(`[WebSocket] ${error.message}`);
 };
